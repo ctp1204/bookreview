91ecf322e0ef62a1521e02f6c13313972444366f
ctp1204 <lqcuong.qt@gmail.com>
2019-03-14 08:40:16 +0700

    admin_book

--- app/helpers/admin/books_helper.rb
+++ app/helpers/admin/books_helper.rb
@@ -1,9 +1,5 @@
-module BooksHelper
+module Admin::BooksHelper
   def load_categories_for_selectbox
     @categories = Category.sort_by_name.map{|c| [c.name, c.id]}
-  end
-
-  def load_categories
-    Category.sort_by_name
   end
 end
